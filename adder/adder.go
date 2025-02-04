c8dd23fef1b949b3d2de29916c754b78e1a4cc60
LowTechTurtle <dongquocthang1235@gmail.com>
2025-02-04 10:46:41 +0700

    update

--- adder/adder.go
+++ adder/adder.go
@@ -1,5 +1,11 @@
+// A package to add numbers
 package adder
 
+// A function to add 2 integer numbers
+// returns the sum of the two provided params
+//
+// An explaination on adding numbers
+// [Add is fun]: https://www.mathsisfun.com/numbers/addition.html
 func AddInt(a int, b int) int {
 	return a + b
 }
