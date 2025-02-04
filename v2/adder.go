3acca3c3342cf5a1af34d650a71665346d2fe951
LowTechTurtle <dongquocthang1235@gmail.com>
2025-02-04 10:56:19 +0700

    update v2

---
+++ v2/adder.go
@@ -0,0 +1,11 @@
+package adder
+
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+func Add[T Number](a, b T) T {
+	return a + b
+}
---
+++ v2/go.mod
@@ -0,0 +1,5 @@
+module github.com/LowTechTurtle/test_go_versioning/v2
+
+go 1.23.5
+
+require golang.org/x/exp v0.0.0-20250128182459-e0ece0dbea4c
